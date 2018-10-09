c0d2c81c6f68ba40bfb4ee046fab982dfae48e19
JulieStuneck <jbstuneck@yahoo.com>
2018-10-09 11:00:01 -0400

    working when urls are hardcoded

--- swapiQuizScript.js
+++ swapiQuizScript.js
@@ -1,5 +1,6 @@
-let randomNumber = Math.floor((Math.random() * 88) * 1)
-let apiUrl = 'https://swapi.co/api/people/' + randomNumber
+/*let randomNumber = Math.floor((Math.random() * 88) * 1)
+let apiUrl = 'https://swapi.co/api/people/' + randomNumber*/
+let apiUrl = 'https://swapi.co/api/people/1/'
 
 let name = document.querySelector('#characterName');
 let gender = document.querySelector('#gender');
@@ -20,9 +21,13 @@ fetch(apiUrl).then(response => {
 	hairColor.innerText = `Hair Color:  ${data.hair_color}`
 	let planetUrl = `${data.homeworld}`
 	let speciesUrl = `${data.species}`
-	starshipUrl = `${[data.starships]}`
+	let starshipUrls = `${data.starships}` 
+	/*let starshipUrls = [
+        "https://swapi.co/api/starships/12/", 
+        "https://swapi.co/api/starships/22/"
+    ]*/
+//let starshipUrl = "https://swapi.co/api/starships/12/" - works without no-cors
 
-	
 
 	fetch(planetUrl).then(response => {
 	return response.json();
@@ -36,23 +41,43 @@ fetch(apiUrl).then(response => {
 		return response.json();		
 	}).then(data => {
 		species.innerText = `Species:  ${data.name}`
-	}).catch(ERR => {
+	}).catch(err => {
 		species.innerText = "Species Not Found"
 	})
 
-	fetch(starshipUrl, {
+	
+	Promise.all(starshipUrls.map(url =>  //works for one ship
+	    fetch(url).then(name => name.json())
+	))
+	  .then(array => {
+	    //starship.innerText = 'Starship(s): ' + array[0].name + ', ' + array[1].name
+	   
+	    for (var i=0; i<starshipUrls.length; i++) {
+	    	let para = document.createElement('p');
+	    	let ship = document.createTextNode(`${array[i].name}, `)
+	    	para.appendChild(ship);
+	    	var element = document.getElementById('starship');
+	    	element.appendChild(ship);
+			//starship.innerText = `Starship(s):  ${array[i].name}`  -works, but only shows last index
+		}
+
+	  }).catch(err => {
+		starship.innerText = 'Starship Not Found'
+	})
+
+	/*fetch(starshipUrls, {
 		mode: 'no-cors'
 	}).then(response => {
 		return response.json();
 	}).then(data => {
+		let starshipArray = [`${data.name}`]
 		
-		for (var i=0; i<arr.length; i++) {
-			starship.innerText = (`Starship:  ${data.name}`)[i]
-		}
+		//starship.innerText = `Starship: ${data.name}`
+		starship.innerText = `Starship: ${data.name}`
 		
 	}).catch(err => {
 		starship.innerText = 'Starship Not Found'
-	})
+	})*/
 	
 }).catch(err => {
  	name.innerText = "Something went wrong. Please, refresh."
